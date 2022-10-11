5e673095fba5f6059e331d75c85db5978a7882c3
Mickeel <mickeelp@gmail.com>
2022-10-11 21:19:17 +0700

    list Post

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,154 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main class="container mx-auto">
+    <div class="flex flex-wrap justify-between pt-12 -mx-6">
+      <div
+        v-for="(post, index) in posts"
+        :key="index"
+        class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink"
+      >
+        <div
+          class="flex-1 flex-row bg-white rounded-t rounded-b-none overflow-hidden shadow-lg"
+        >
+          <a href="#" class="flex flex-wrap no-underline hover:no-underline">
+            <img
+              :src="`https://placeimg.com/400/320/people?index=${index}`"
+              class="h-full w-full rounded-t pb-6"
+            />
+            <div class="w-full font-bold text-xl text-gray-900 px-6">
+              {{ post.title }}
+            </div>
+            <p class="text-gray-800 font-serif text-base px-6 mb-5 truncate">
+              {{ post.content }}
+            </p>
+          </a>
+        </div>
+        <div
+          class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6"
+        >
+          <div class="flex items-center justify-between">
+            <img
+              class="w-8 h-8 rounded-full mr-4 avatar"
+              data-tippy-content="{{ post.leaderName }}"
+              :src="getAvatar(post.leaderPicUrl, post.leaderName)"
+              alt="{{ post.leaderName }}"
+              tabindex="0"
+            />
+            <p class="w-full text-gray-600 text-xs md:text-sm">
+              {{ post.leaderName }}
+            </p>
+            <p class="text-gray-600 text-xs">
+              {{ dateTime(post.date) }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Pagination />
   </main>
 </template>
+
+<script setup>
+import Pagination from "./../components/PaginationBar.vue";
+</script>
+
+<script>
+import axios from "axios";
+import moment from "moment";
+
+export default {
+  data() {
+    return {
+      search: "",
+      searchResults: [],
+      posts: [],
+      leaders: [],
+      author: "",
+      apiUrl: import.meta.env.VITE_GRAPHQL_API_URL,
+      apiKey: import.meta.env.VITE_GRAPHQL_API_KEY,
+    };
+  },
+
+  mounted() {
+    this.getPost();
+  },
+
+  methods: {
+    getPost() {
+      const body = {
+        query: `
+          query postByCategory {
+            postByCategory(category: POST, limit: 9) {
+              items {
+                id
+                title
+                dateTo
+                leaderID
+                status
+                category
+                content
+              }
+              nextToken
+              startedAt
+            }
+          }
+        `,
+        variables: {},
+      };
+      const options = {
+        headers: {
+          "x-api-key": this.apiKey,
+        },
+      };
+      axios.post(this.apiUrl, body, options).then((response) => {
+        this.searchResults = response.data.data.postByCategory.items;
+        this.searchResults.forEach((item) => {
+          this.posts.push({
+            leaderID: item.leaderID,
+            leaderName: "-",
+            leaderPicUrl: null,
+            title: item.title,
+            content: item.content,
+            date: item.dateTo,
+          });
+        });
+        console.log(this.posts);
+
+        this.posts.forEach((item) => {
+          const body = {
+            query: `
+            query getLeader {
+              getLeader(id: "${item.leaderID}") {
+                id
+                name
+                path
+                picUrl
+              }
+            }
+          `,
+            variables: {},
+          };
+          const options = {
+            headers: {
+              "x-api-key": this.apiKey,
+            },
+          };
+          axios.post(this.apiUrl, body, options).then((response) => {
+            item.leaderName = response.data.data.getLeader.name;
+            item.leaderPicUrl = response.data.data.getLeader.picUrl;
+          });
+        });
+        console.log(this.posts);
+      });
+    },
+    dateTime(value) {
+      return moment(value).format("MMM D YY");
+    },
+    getAvatar(url, name) {
+      if (url == null) {
+        return "http://i.pravatar.cc/300?index=${name}";
+      }
+      return url;
+    },
+  },
+};
+</script>
